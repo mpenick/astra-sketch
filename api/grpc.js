1f883b86e6cf2cb1fc0ad0c2aaad991167ea8951
Michael Penick <michael.penick@datastax.com>
2021-11-22 15:51:44 -0500

    Finished adding most of the backend API. Maybe buggy

--- api/grpc.js
+++ api/grpc.js
@@ -18,9 +18,10 @@ async function createSchema() {
   await client.executeQuery(new sg.Query().setCql(
     `CREATE TABLE IF NOT EXISTS ${keyspace}.game 
       (game_id text, 
-       vip text, 
+       game_num int,
+       vip uuid, 
        state text, 
-       topic text, 
+       topic_player_id uuid, 
        PRIMARY KEY(game_id))`));
   await client.executeQuery(new sg.Query().setCql(
     `CREATE TABLE IF NOT EXISTS ${keyspace}.players 
@@ -31,23 +32,25 @@ async function createSchema() {
   await client.executeQuery(new sg.Query().setCql(
     `CREATE TABLE IF NOT EXISTS ${keyspace}.topics 
       (game_id text, 
-       player_id uuid,
+       game_num int,
+       topic_player_id uuid,
        topic text,
-       svg text, 
-       PRIMARY KEY(game_id, player_id, topic))`));
+       svg text,
+       PRIMARY KEY(game_id, game_num, topic_player_id))`));
   await client.executeQuery(new sg.Query().setCql(
     `CREATE TABLE IF NOT EXISTS ${keyspace}.guesses 
       (game_id text, 
+       game_num int,
+       topic_player_id uuid,
        player_id uuid,
-       topic text,
        guess text,
-       votes list<text>,
-       PRIMARY KEY(game_id, player_id, topic))`));
+       votes set<uuid>,
+       PRIMARY KEY(game_id, game_num, topic_player_id, player_id))`));
 }
 
 async function getGame(gameId) {
   const gameQuery = new sg.Query();
-  gameQuery.setCql(`SELECT JSON * FROM ${keyspace}.game WHERE game_id = '${gameId}'`);
+  gameQuery.setCql(`SELECT JSON game_id, game_num, vip, topic_player_id, state FROM ${keyspace}.game WHERE game_id = '${gameId}'`);
 
   const playerQuery = new sg.Query();
   playerQuery.setCql(`SELECT JSON player_id, name FROM ${keyspace}.players WHERE game_id = '${gameId}'`);
@@ -57,6 +60,7 @@ async function getGame(gameId) {
 
   if (gameRes.hasResultSet() && gameRes.getResultSet().getRowsList()) {
     const game = JSON.parse(gameRes.getResultSet().getRowsList()[0].getValuesList()[0].getString());
+    const gameNum = game.game_num;
 
     const players = [];
     if (playerRes.hasResultSet() && playerRes.getResultSet().getRowsList()) {
@@ -64,6 +68,32 @@ async function getGame(gameId) {
         players.push(JSON.parse(row.getValuesList()[0].getString()));
       });
       game.players = players;
+    }
+
+    const topicsQuery = new sg.Query();
+    topicsQuery.setCql(`SELECT JSON topic_player_id, topic, svg FROM ${keyspace}.topics WHERE game_id = '${gameId}' AND game_num = ${gameNum}`);
+
+    const topicsRes = await client.executeQuery(topicsQuery);
+
+    const topics = [];
+    if (topicsRes.hasResultSet() && topicsRes.getResultSet().getRowsList()) {
+      topicsRes.getResultSet().getRowsList().forEach(row => {
+        topics.push(JSON.parse(row.getValuesList()[0].getString()));
+      });
+      game.topics = topics;
+    }
+
+    const guessesQuery = new sg.Query();
+    guessesQuery.setCql(`SELECT JSON topic_player_id, player_id, guess, votes FROM ${keyspace}.guesses WHERE game_id = '${gameId}' AND game_num = ${gameNum}`);
+
+    const guessesRes = await client.executeQuery(guessesQuery);
+
+    const guesses = [];
+    if (guessesRes.hasResultSet() && guessesRes.getResultSet().getRowsList()) {
+      guessesRes.getResultSet().getRowsList().forEach(row => {
+        guesses.push(JSON.parse(row.getValuesList()[0].getString()));
+      });
+      game.guesses = guesses;
     }
 
     return game;
--- api/routes/games.js
+++ api/routes/games.js
@@ -1,40 +1,50 @@
 var sg = require('@stargate-oss/stargate-grpc-node-client')
 var grpc = require('../grpc')
 var config = require('../config')
+const uuid = require('uuid');
 var express = require('express');
 var { publishGameChange } = require('../pulsar')
 
 var router = express.Router();
 
-var grpc = require("../grpc")
+var grpc = require("../grpc");
+const { generateTopic } = require('../../ui/src/utils');
 
+// Get full game state object
 router.get('/:id', function(req, res, next) {
     grpc.getGame(req.params.id).then(game => {
-        res.status(200).write(JSON.stringify(game));
+        res.status(200).write(JSON.stringify(game, null, 2));
         res.end();
     }).catch(err => {
         res.status(500).render('error', { message: "Error attempting to get game", error: err });
     });
 });
 
-router.put('/:id', function(req, res, next) {
+// New game
+router.post('/:id', function(req, res, next) {
     if (req.body.vip) {
+        const vip = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(uuid.v4())));
         const id = new sg.Value().setString(req.params.id);
-        const vip = new sg.Value().setString(req.body.vip);
-        const values = new sg.Values();
-        values.addValues(id);
-        values.addValues(vip);
+        const name = new sg.Value().setString(req.body.vip);
 
         const batch = new sg.Batch();
+        batch.setType(sg.Batch.Type.LOGGED);
 
         const insertGame = new sg.Query();
-        insertGame.setCql(`INSERT INTO ${config.KEYSPACE}.game (game_id, vip, state) VALUES (?, ?, 'NEW_GAME')`);
-        insertGame.setValues(values);
+        const insertGameValues = new sg.Values();
+        insertGameValues.addValues(id);
+        insertGameValues.addValues(vip);
+        insertGame.setCql(`INSERT INTO ${config.KEYSPACE}.game (game_id, game_num, vip, state) VALUES (?, 0, ?, 'NEW_GAME')`);
+        insertGame.setValues(insertGameValues);
         batch.addQueries(insertGame);
 
         const insertPlayer = new sg.Query();
-        insertPlayer.setCql(`INSERT INTO ${config.KEYSPACE}.players (game_id, player_id, name) VALUES (?, uuid(), ?)`);
-        insertPlayer.setValues(values);
+        const insertPlayerValues = new sg.Values();
+        insertPlayerValues.addValues(id);
+        insertPlayerValues.addValues(vip);
+        insertPlayerValues.addValues(name);
+        insertPlayer.setCql(`INSERT INTO ${config.KEYSPACE}.players (game_id, player_id, name) VALUES (?, ?, ?)`);
+        insertPlayer.setValues(insertPlayerValues);
         batch.addQueries(insertPlayer);
 
         grpc.client.executeBatch(batch).then((response) => {
@@ -49,9 +59,51 @@ router.put('/:id', function(req, res, next) {
     }
 });
 
-router.put('/:id/players/:name', function(req, res, next) {
+// Update topic
+router.put('/:id/topic/:topicPlayerId', function(req, res, next) {
+    const id = new sg.Value().setString(req.params.id);
+    const topicPlayerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(req.params.topicPlayerId)));
+
+    const query = new sg.Query();
+    const values = new sg.Values();
+    values.addValues(topicPlayerId);
+    values.addValues(id);
+    query.setCql(`UPDATE ${config.KEYSPACE}.game SET topic_player_id = ? WHERE game_id = ?`);
+    query.setValues(values);
+
+    grpc.client.executeQuery(query).then((response) => {
+        publishGameChange(req.params.id);
+        res.status(200).end();
+    }).catch((err) => {
+        res.status(500).render('error', { message: "Error attempting to update topic", error: err });
+    });
+});
+
+// Update topic
+router.put('/:id/state/:state', function(req, res, next) {
+    const id = new sg.Value().setString(req.params.id);
+    const state = new sg.Value().setString(req.params.state);
+
+    const query = new sg.Query();
+    const values = new sg.Values();
+    values.addValues(state);
+    values.addValues(id);
+    query.setCql(`UPDATE ${config.KEYSPACE}.game SET state = ? WHERE game_id = ?`);
+    query.setValues(values);
+
+    grpc.client.executeQuery(query).then((response) => {
+        publishGameChange(req.params.id);
+        res.status(200).end();
+    }).catch((err) => {
+        res.status(500).render('error', { message: "Error attempting to update topic", error: err });
+    });
+});
+
+// Add player
+router.post('/:id/players/:name', function(req, res, next) {
     const id = new sg.Value().setString(req.params.id);
     const name = new sg.Value().setString(req.params.name);
+
     const values = new sg.Values();
     values.addValues(id);
     values.addValues(name);
@@ -65,6 +117,159 @@ router.put('/:id/players/:name', function(req, res, next) {
     }).catch((err) => {
         res.status(500).render('error', { message: "Error attempting to add new player", error: err });
     });
+});
+
+
+// New game instance
+//
+// Input: Takes an array of player uuid strings
+//  {
+//    "players": [ "2f516cb5-a357-4b11-b71d-592d5d8fe406", "a5c632f5-36c1-4061-8791-1380e81b8c49" ]
+//  }
+router.post('/:id/instances/:num', function(req, res, next) {
+    if (req.body.players && Array.isArray(req.body.players)) {
+        const batch = new sg.Batch();
+        batch.setType(sg.Batch.Type.LOGGED);
+        let topics = [];
+        req.body.players.forEach(player => {
+            try {
+                let topic = generateTopic(topics);
+                topics.push(topic);
+
+                const id = new sg.Value().setString(req.params.id);
+                const num = new sg.Value().setInt(req.params.num);
+                const playerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(player)));
+                const topicVal = new sg.Value().setString(topic);
+
+                const values = new sg.Values();
+                values.addValues(id);
+                values.addValues(num);
+                values.addValues(playerId);
+                values.addValues(topicVal);
+
+                const query = new sg.Query();
+                query.setCql(`INSERT INTO ${config.KEYSPACE}.topics (game_id, game_num, topic_player_id, topic) VALUES (?, ?, ?, ?)`);
+                query.setValues(values);
+
+                batch.addQueries(query);
+            } catch(e) {
+                console.log(`Unable to add player ${player}`);
+            }
+        });
+
+        grpc.client.executeBatch(batch).then((response) => {
+            publishGameChange(req.params.id);
+            res.status(200).end();
+        }).catch((err) => {
+            res.status(500).render('error', { message: "Error attempting to add new game instance", error: err });
+        });
+    } else {
+        res.status(400).render( 'error', { message: "'players' array not provided", error: new Error("'players' array not provided")})
+    }
+});
+
+
+// Upload sketch svg
+//
+// Input: Takes an string with svg data
+//  {
+//    "svg": "somesvgdata"
+//  }
+router.put('/:id/instances/:num/topics/:playerId/svg', function(req, res, next) {
+    if (req.body.svg) {
+        const id = new sg.Value().setString(req.params.id);
+        const num = new sg.Value().setInt(req.params.num);
+        const playerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(req.params.playerId)));
+        const svg = new sg.Value().setString(req.body.svg);
+
+        const values = new sg.Values();
+        values.addValues(id);
+        values.addValues(num);
+        values.addValues(playerId);
+        values.addValues(svg);
+
+        const query = new sg.Query();
+        query.setCql(`INSERT INTO ${config.KEYSPACE}.topics (game_id, game_num, topic_player_id, svg) VALUES (?, ?, ?, ?)`);
+        query.setValues(values);
+
+        grpc.client.executeQuery(query).then((response) => {
+            publishGameChange(req.params.id);
+            res.status(200).end();
+        }).catch((err) => {
+            res.status(500).render('error', { message: "Error attempting to update svg on topic", error: err });
+        });
+    } else {
+        res.status(400).render('error', { message: "'svg' not provided", error: new Error("'svg' not provided") })
+    }
+});
+
+// Add guess
+//
+// Input: Takes an string with the players guess
+//  {
+//    "guess": "Wacky Guess"
+//  }
+router.post('/:id/instances/:num/guesses/:topicPlayerId/guess/:playerId', function(req, res, next) {
+    if (req.body.guess) {
+        const id = new sg.Value().setString(req.params.id);
+        const num = new sg.Value().setInt(req.params.num);
+        const playerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(req.params.playerId)));
+        const topicPlayerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(req.params.topicPlayerId)));
+        const guess = new sg.Value().setString(req.body.guess);
+
+        const values = new sg.Values();
+        values.addValues(id);
+        values.addValues(num);
+        values.addValues(playerId);
+        values.addValues(topicPlayerId);
+        values.addValues(guess);
+
+        const query = new sg.Query();
+        query.setCql(`INSERT INTO ${config.KEYSPACE}.guesses (game_id, game_num, topic_player_id, player_id, guess) VALUES (?, ?, ?, ?, ?)`);
+        query.setValues(values);
+
+        grpc.client.executeQuery(query).then((response) => {
+            publishGameChange(req.params.id);
+            res.status(200).end();
+        }).catch((err) => {
+            res.status(500).render('error', { message: "Error attempting to add guess", error: err });
+        });
+    } else {
+        res.status(400).render('error', { message: "'guess' not provided", error: new Error("'guess' not provided") })
+    }
+});
+
+// Vote
+// Input: Takes an string with the current player's ID who is voting
+//  {
+//    "player": "2f516cb5-a357-4b11-b71d-592d5d8fe406"
+//  }
+router.post('/:id/instances/:num/guesses/:topicPlayerId/vote/:guessPlayerId/', function(req, res, next) {
+    if (req.body.player) {
+        const id = new sg.Value().setString(req.params.id);
+        const num = new sg.Value().setInt(req.params.num);
+        const topicPlayerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(req.params.topicPlayerId)));
+        const guessPlayerId = new sg.Value().setUuid(new sg.Uuid().setValue(uuid.parse(req.params.guessPlayerId)));
+
+        const values = new sg.Values();
+        values.addValues(id);
+        values.addValues(num);
+        values.addValues(topicPlayerId);
+        values.addValues(guessPlayerId);
+
+        const query = new sg.Query();
+        query.setCql(`UPDATE ${config.KEYSPACE}.guesses SET votes = votes + {${req.body.player}} WHERE game_id = ? AND game_num = ? AND topic_player_id = ? AND player_id = ?`);
+        query.setValues(values);
+
+        grpc.client.executeQuery(query).then((response) => {
+            publishGameChange(req.params.id);
+            res.status(200).end();
+        }).catch((err) => {
+            res.status(500).render('error', { message: "Error attempting to add vote", error: err });
+        });
+    } else {
+        res.status(400).render('error', { message: "'player' not provided", error: new Error("'player' not provided") })
+    }
 });
 
 module.exports = router;
