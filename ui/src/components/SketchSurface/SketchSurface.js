7588f7ace7a7bf5ace9d010c8bb2c96b0c62f756
Michael Penick <michael.penick@datastax.com>
2021-11-22 11:44:44 -0500

    Simple sketch surface with a palette

--- ui/src/components/SketchSurface/SketchSurface.js
+++ ui/src/components/SketchSurface/SketchSurface.js
@@ -8,50 +8,95 @@ const styles = {
   borderRadius: "0",
 };
 
-const SketchSurface = () => {
-  const canvasRef = React.createRef();
+const SketchSurface = class extends React.Component {
+  state = { strokeColor: "rgb(104, 166, 116)", debug: false };
 
-  const colorSelected = (e) => {
-    console.log('clicked ' + JSON.stringify(canvasRef.current));
-  };
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
 
+  colorSelected = e => {
+    const color = window.getComputedStyle(e.target, null).getPropertyValue('background-color');
+    this.setState({strokeColor: color});
+    console.log("color selected");
+  }
 
-  // https://github.com/vinothpandian/react-sketch-canvas#list-of-props
-  return (
-    <Grid container justifyContent="center">
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        style={{ width: 320 }}
-        spacing={2}
-      >
-        <Grid item>
-          <ReactSketchCanvas
-            name="sketch"
-            ref={canvasRef}
-            style={styles}
-            width="400px"
-            height="400px"
-            strokeWidth={4}
-            strokeColor="red"
-          />
-        </Grid>
+  getSvg = () => {
+    return this.canvasRef.current.exportSvg();
+  }
+
+  clear = () => {
+    this.canvasRef.current.clearCanvas();
+  }
+
+  render() {
+    let debugButtons;
+    if (this.state.debug) {
+      debugButtons =
         <Grid item>
           <Grid container spacing={1}>
             <Grid item>
-              <input name="orange" className="btn" type="button" onClick={colorSelected} />
+              <button onClick={() => {
+                this.getSvg().then(s => {
+                  console.log(s);
+                });
+              }}>SVG</button>
+            </Grid>
+            <Grid item>
+              <button onClick={() => {
+                this.clear();
+              }}>Clear</button>
             </Grid>
           </Grid>
+        </Grid>;
+    }
+
+    return (
+      <Grid container justifyContent="center">
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          style={{ width: 320 }}
+          spacing={2}
+        >
+          <Grid item>
+            <ReactSketchCanvas
+              name="sketch"
+              ref={this.canvasRef}
+              style={styles}
+              width="400px"
+              height="400px"
+              strokeWidth={4}
+              strokeColor={this.state.strokeColor}
+            />
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item>
+                <input className="btn-green" type="button" onClick={this.colorSelected} />
+              </Grid>
+              <Grid item>
+                <input className="btn-blue" type="button" onClick={this.colorSelected} />
+              </Grid>
+              <Grid item>
+                <input className="btn-lightblue" type="button" onClick={this.colorSelected} />
+              </Grid>
+              <Grid item>
+                <input className="btn-yellow" type="button" onClick={this.colorSelected} />
+              </Grid>
+              <Grid item>
+                <input className="btn-orange" type="button" onClick={this.colorSelected} />
+              </Grid>
+            </Grid>
+          </Grid>
+          {debugButtons}
         </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
 };
-
-SketchSurface.propTypes = {};
-
-SketchSurface.defaultProps = {};
 
 export default SketchSurface;
